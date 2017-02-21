df3d946c050aaaf2af68499eef9166337c388c93
Andika Priantama <andika.priantama@mitrais.com>
2017-02-21 16:40:01 +0700

    Change deployment group and application name

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,8 +4,8 @@ import groovy.json.JsonSlurperClassic
 node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
-	def applicationName = 'fajar-app' //change me
-	def deploymentGroupName = 'fajar-app' // change me
+	def applicationName = 'langit-cdc' //change me
+	def deploymentGroupName = 'andika' // change me
 	def s3BucketName = 'deployment-cdc'
 	
 	//Cleanup workspace
